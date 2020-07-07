90632bde0cab0b0d224e9df9568fa44136b0b124
Andrew Dina <andrewthedina@icloud.com>
2020-07-07 10:54:33 -0400

    Creates two options for Picker optional selection

--- SwiftUIPickerWithOptionalSelection/ContentView.swift
+++ SwiftUIPickerWithOptionalSelection/ContentView.swift
@@ -9,8 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: String? = nil
+    
+    let items = ["Item A", "Item B", "Item C"]
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Picker(selection: $selection, label: Text("Picker with optional binding")) {
+                    /* Begin option 1: nil by SELECTION */
+                    
+                    Text("[none]").tag(nil as String?)
+                    
+                    /* End option 1 */
+
+                    ForEach(items, id: \.self) { item in
+                        Text(item).tag(item as String?)
+                        // Tags must be cast to same type as Picker selection ($selection)
+                    }
+                }
+                /* Begin option 2: NIL BY BUTTON ACTION */
+                
+//                if selection != nil { // "Remove item" button only appears if selection is not nil
+//                    Button("Remove item") {
+//                        self.selection = nil
+//                    }
+//                }
+                
+                /* End Options 2 */
+            }
+        }
     }
 }
 
